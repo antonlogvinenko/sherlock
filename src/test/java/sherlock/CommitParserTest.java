cd87ef7c66afac4f168dab64153f22e0f19ddb95
Anton Logvinenko <anton.logvinenko@gmail.com>
2015-09-11 22:01:01 +0300

    Test for SVN commit parser

--- src/test/java/sherlock/CommitParserTest.java
+++ src/test/java/sherlock/CommitParserTest.java
@@ -1,5 +1,17 @@
 package sherlock;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CommitParserTest {
 
+	public void testA() throws IOException {
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream("migration-task.diff");
+		List<String> commits = new CommitParser(is).getCommits().collect(Collectors.toList());
+		System.out.println(commits);
+	}
 }
