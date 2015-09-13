1b52a118ba70bbdcaa523bc224e30ffd8ea045cf
Anton Logvinenko <anton.logvinenko@gmail.com>
2015-09-13 14:54:18 +0300

    Fix repository

--- src/main/java/sherlock/App.java
+++ src/main/java/sherlock/App.java
@@ -4,7 +4,6 @@ import sherlock.commit.Repository;
 
 import java.io.IOException;
 
-
 public class App {
 
 	public static void main(String[] args) throws IOException {
--- src/main/java/sherlock/commit/Repository.java
+++ src/main/java/sherlock/commit/Repository.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import static java.lang.Runtime.getRuntime;
 import static java.lang.String.format;
-import static java.lang.Thread.currentThread;
 import static java.util.stream.Collectors.toSet;
 
 public class Repository {
@@ -22,10 +21,6 @@ public class Repository {
 			commit2 = commit2 == null || commit2.isEmpty() ? "HEAD" : commit2;
 			String cmd = format("svn diff -r %s:%s %s", commit1, commit2, repository);
 			Process p = getRuntime().exec(cmd);
-			int exitCode = p.waitFor();
-			if (exitCode != 0) {
-				throw new RuntimeException(format("Command %s terminated with error code %s", cmd, exitCode));
-			}
 			return new Commit(p.getInputStream())
 				.getCommitChunks()
 				.flatMap(CommitSignificantContent::getSignificantLines)
@@ -34,9 +29,6 @@ public class Repository {
 				.flatMap(CommitSignificantContent::enrich)
 				.collect(toSet());
 		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} catch (InterruptedException e) {
-			currentThread().interrupt();
 			throw new RuntimeException(e);
 		}
 	}
